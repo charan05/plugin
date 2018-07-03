ce8b992ca715cfee5c924d3d5135d08f59718e07
charan05 <ch12jan1998@gmail.com>
2018-07-03 10:53:20 +0530

    first commit

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+   /*
+   Plugin Name: Printing a line
+   description: >-
+   A plugin to print a line
+   Version: 1.0
+   Author: Charan Sai Karthick M
+   */
+   echo "Hello World!!";
+?>
